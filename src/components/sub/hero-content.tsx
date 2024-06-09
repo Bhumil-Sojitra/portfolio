fb35c0db2420286d6b4cee0d369d756ba6ef05ef
Bhumil-Sojitra <bhumilsojitra17@gmail.com>
2024-06-09 12:03:15 +0530

    hero design

--- src/components/sub/hero-content.tsx
+++ src/components/sub/hero-content.tsx
@@ -13,9 +13,9 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+      className="flex flex-row items-center justify-center px-10 mt-40 w-full z-[20]"
     >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto items-center">
         <motion.div
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
@@ -49,7 +49,7 @@ const HeroContent = () => {
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+          className="py-2 px-3 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
           Learn more!
         </motion.a>
